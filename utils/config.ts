c15c236dcf1f0cdd9c104184c59bed3fe8e7559f
shenger <714275326@qq.com>
2022-06-06 16:43:20 +0800

    add token list

--- utils/config.ts
+++ utils/config.ts
@@ -6,9 +6,9 @@ export const MONGO_PORT = 27017;
 export const REDIS_PORT = 6379;
 
 export type TFrom = 'market' | 'chain';
-export type TTokens = 'KAR' | 'KSM' | 'BNC' | 'DOT' | 'PHA' | 'RMRK' | 'PCX' | 'EDG' | 'ACA';
+export type TTokens = 'KAR' | 'KSM' | 'BNC' | 'DOT' | 'PHA' | 'RMRK' | 'PCX' | 'EDG' | 'ACA' | 'KINT' | 'QTZ'| 'MOVR' | 'USDT' | 'WBNB' | 'BTC' | 'CSM' | 'USDC';
 
-export const ALLOW_TOKENS: (TTokens | string)[] = ['KAR', 'KSM', 'BNC', 'DOT', 'PHA', 'RMRK', 'PCX', 'EDG', 'ACA'];
+export const ALLOW_TOKENS: (TTokens | string)[] = ['KAR', 'KSM', 'BNC', 'DOT', 'PHA', 'RMRK', 'PCX', 'EDG', 'ACA', 'KINT', 'QTZ', 'MOVR', 'USDT', 'WBNB', 'BTC', 'CSM', 'USDC'];
 export const TOKENS_MAP: { [k in (TTokens | string)]: number } = {
   KAR: 5034,
   KSM: 10042,
@@ -18,7 +18,15 @@ export const TOKENS_MAP: { [k in (TTokens | string)]: number } = {
   RMRK: 12140,
   PCX: 4200,
   EDG: 5274,
-  ACA: 6756
+  ACA: 6756,
+  KINT: 13675,
+  QTZ: 13685,
+  MOVR: 9285,
+  USDT: 825,
+  WBNB: 7192,
+  BTC: 1,
+  CSM: 10055,
+  USDC: 3408
 }
 export const CMC_API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
 export const CMC_API_URL_EXCHANGE = "https://pro-api.coinmarketcap.com/v1/tools/price-conversion";
