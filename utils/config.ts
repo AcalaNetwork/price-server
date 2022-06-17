97d35d142c3a34df8e387e3f5db46e1f2c146e7d
shenger <714275326@qq.com>
2022-06-17 11:21:02 +0800

    add 4 tokens

--- utils/config.ts
+++ utils/config.ts
@@ -6,9 +6,9 @@ export const MONGO_PORT = 27017;
 export const REDIS_PORT = 6379;
 
 export type TFrom = 'market' | 'chain';
-export type TTokens = 'KAR' | 'KSM' | 'BNC' | 'DOT' | 'PHA' | 'RMRK' | 'PCX' | 'EDG' | 'ACA' | 'KINT' | 'QTZ'| 'MOVR' | 'USDT' | 'WBNB' | 'BTC' | 'CSM' | 'USDC';
+export type TTokens = 'KAR' | 'KSM' | 'BNC' | 'DOT' | 'PHA' | 'RMRK' | 'PCX' | 'EDG' | 'ACA' | 'KINT' | 'QTZ'| 'MOVR' | 'USDT' | 'WBNB' | 'BTC' | 'CSM' | 'USDC' | 'GLMR' | 'KMA' | 'TEER' | 'CRAB';
 
-export const ALLOW_TOKENS: (TTokens | string)[] = ['KAR', 'KSM', 'BNC', 'DOT', 'PHA', 'RMRK', 'PCX', 'EDG', 'ACA', 'KINT', 'QTZ', 'MOVR', 'USDT', 'WBNB', 'BTC', 'CSM', 'USDC'];
+export const ALLOW_TOKENS: (TTokens | string)[] = ['KAR', 'KSM', 'BNC', 'DOT', 'PHA', 'RMRK', 'PCX', 'EDG', 'ACA', 'KINT', 'QTZ', 'MOVR', 'USDT', 'WBNB', 'BTC', 'CSM', 'USDC', 'GLMR', 'KMA', 'TEER', 'CRAB'];
 export const TOKENS_MAP: { [k in (TTokens | string)]: number } = {
   KAR: 5034,
   KSM: 10042,
@@ -26,7 +26,11 @@ export const TOKENS_MAP: { [k in (TTokens | string)]: number } = {
   WBNB: 7192,
   BTC: 1,
   CSM: 10055,
-  USDC: 3408
+  USDC: 3408,
+  GLMR: 6836,
+  KMA: 15305,
+  TEER: 13323,
+  CRAB: 9243
 }
 export const CMC_API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
 export const CMC_API_URL_EXCHANGE = "https://pro-api.coinmarketcap.com/v1/tools/price-conversion";
